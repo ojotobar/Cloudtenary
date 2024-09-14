24f5c931cc835b8e25f914e539e4c7bbf0e7fb99
Toba Ojo <tojo@axxess.com>
2024-09-14 15:36:03 +0100

    Added the method implementations to the interface

--- ICloudtenary.cs
+++ ICloudtenary.cs
@@ -1,6 +1,12 @@
-﻿namespace Cloudtenary
+﻿using CloudinaryDotNet.Actions;
+using Cloudtenary.Models;
+
+namespace Cloudtenary
 {
     public interface ICloudtenary
     {
+        Task<CloudtenaryUploadResult?> UploadImage(string fileName, Stream stream, int width, int height);
+        Task<CloudtenaryUploadResult?> UploadImage(string fileName, Stream stream);
+        Task<bool> DeleteMediaFile(string id, ResourceType type);
     }
 }
